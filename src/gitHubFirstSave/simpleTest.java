7218606d01eb240f3ec0edfd63acfa4404d49ced
ihorp <IhorPetrytskyi@gmail.com>
2019-11-09 15:05:23 -0600

    hi+1+1+newText

--- src/gitHubFirstSave/simpleTest.java
+++ src/gitHubFirstSave/simpleTest.java
@@ -4,4 +4,5 @@ public class simpleTest {
     public static void main(String[] args) {
         System.out.println("dfdf world");
         System.out.println("xz");
+        System.out.println("new text");
 }}
